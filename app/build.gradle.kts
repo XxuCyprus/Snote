331e2ccafa4ed835fe4829b1e18d5107d6538f5a
XxuCyprus <Xx03077@163.com>
2026-07-25 03:29:07 +0800

    修复：使用 import java.util.Properties 解决引用问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -6,7 +8,7 @@ plugins {
     id("com.google.devtools.ksp")
 }
 
-val localProperties = java.util.Properties()
+val localProperties = Properties()
 val localPropertiesFile = rootProject.file("local.properties")
 if (localPropertiesFile.exists()) {
     localProperties.load(localPropertiesFile.inputStream())
